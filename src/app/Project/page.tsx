2a8fcbbd8290e28f6f818fbae72324cba31e6ffc
Sankalp <sankalpppppp@gmail.com>
2024-07-12 13:08:00 +0530

    Implement dynamic content loading

--- src/app/Project/page.tsx
+++ src/app/Project/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
 import Satoshi_Regular from 'next/font/local'
 import Satoshi_Bold from 'next/font/local'
 import CabinetGrotesk from 'next/font/local'
@@ -13,167 +13,142 @@ import { ArrowUpRight } from 'lucide-react'
 import { Badge } from '@/components/ui/badge'
 import Link from 'next/link'
 
-const font_2 = Satoshi_Regular({
+const font_1 = Satoshi_Regular({
   src: '../../../fonts/Satoshi-Regular.woff'
 })
-const font_3 = CabinetGrotesk({
+const font_2 = CabinetGrotesk({
   src: '../../../fonts/CabinetGrotesk-Extrabold.woff'
 })
 
-const font_5 = Satoshi_Bold({
+const font_3 = Satoshi_Bold({
   src: '../../../fonts/Satoshi-Bold.woff'
 })
+const cardContent = [
+  {
+    title: 'Simon Game',
+    badge_1: 'Recent',
+    badge_2: 'Implemented',
+    imgSrc: simon,
+    cardDescription:
+      'The project is inspired by the real-world Simon game and tries to replicate all the game conditions with the help of jQuery.',
+    link: 'https://rb.gy/vxr5kv'
+  },
+  {
+    title: 'AllSpace',
+    badge_1: 'Recent',
+    badge_2: 'Implemented',
+    imgSrc: allspace,
+    cardDescription:
+      'It is a documentaion platform, curating the real-time cursor features of google docs and UI interaction of Notion.',
+    link: 'https://rb.gy/drdbtt'
+  },
+  {
+    title: 'TickTick',
+    badge_1: 'Earlier',
+    badge_2: 'Implemented',
+    imgSrc: ticktick,
+    cardDescription:
+      'It is a console level productivity platform that lets you add task, delete them, mark the task as completed and also provides a pomodoro timer.',
+    link: 'https://rb.gy/1w8oko'
+  },
+  {
+    title: 'Weather Application',
+    badge_1: 'Recent',
+    badge_2: 'Implemented',
+    cardDescription:
+      'It is a python application made to understand the working of API. OpenWeather API was used to generate a response and was displayed over a UI made with Tkinter python module.'
+  }
+]
 
-const page = () => {
+const Page = () => {
   return (
     <section className='m-8'>
       <div className='flex h-full w-full items-center justify-center py-10'>
-        <div className='grid h-full place-items-center gap-6 lg:w-[80%] xl:grid-cols-2 2xl:grid-cols-3'>
-          <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
-            <Image
-              src={simon}
-              alt='image'
-              className='h-1/2 rounded-t-2xl object-cover'
-            />
-            <div className='relative h-1/2 px-6 text-[#FFF5EB]'>
-              <h1 className={cn('mt-10 text-5xl ', font_3.className)}>
-                Simon Game
-              </h1>
-              <span
-                className={cn(
-                  'my-2 flex items-center justify-start gap-1',
-                  font_5.className
-                )}
-              >
-                <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>Recent</Badge>
-                <Badge className='border border-[#FFF5EB] bg-transparent'>
-                  Implemented
-                </Badge>
-              </span>
-              <p className={cn('mt-2 text-2xl', font_2.className)}>
-                The project is inspired by the real-world Simon game and tries
-                to replicate all the game conditions with the help of jQuery.
-              </p>
-              <div className='absolute bottom-16 flex items-center justify-start'>
-                <Button
-                  className={cn(
-                    'flex items-center justify-center gap-1 bg-[#222222] p-4 text-xl',
-                    font_2.className
-                  )}
-                >
-                  <Link href='https://rb.gy/vxr5kv'>Link</Link>
-                  <ArrowUpRight size={25} />
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
-            <Image
-              src={allspace}
-              alt='image'
-              className='h-1/2 rounded-t-3xl object-cover'
-            />
-            <div className='relative h-1/2 px-6 text-[#FFF5EB]'>
-              <h1 className={cn('mt-10 text-5xl', font_3.className)}>
-                AllSpace
-              </h1>
-              <span
-                className={cn(
-                  'my-2 flex items-center justify-start gap-1',
-                  font_5.className
-                )}
-              >
-                <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>Recent</Badge>
-                <Badge className='border border-[#FFF5EB] bg-transparent'>
-                  Implemented
-                </Badge>
-              </span>
-              <p className={cn('mt-2 text-2xl ', font_2.className)}>
-                It is a documentaion platform, curating the real-time cursor
-                features of google docs and UI interaction of Notion.
-              </p>
-              <div className='absolute bottom-16 flex items-center justify-start'>
-                <Button
-                  className={cn(
-                    'flex items-center justify-center gap-1 bg-[#222222] p-4 text-xl',
-                    font_2.className
-                  )}
-                >
-                  <Link href='https://rb.gy/drdbtt'>Link</Link>
-                  <ArrowUpRight size={25} />
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
-            <Image
-              src={ticktick}
-              alt='image'
-              className='h-1/2 rounded-t-3xl object-cover'
-            />
-            <div className='relative h-1/2 px-6 text-[#FFF5EB]'>
-              <h1 className={cn('mt-10 text-5xl ', font_3.className)}>
-                TickTick
-              </h1>
-              <span
-                className={cn(
-                  'my-2 flex items-center justify-start gap-1',
-                  font_5.className
-                )}
-              >
-                <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>Earlier</Badge>
-                <Badge className='border border-[#FFF5EB] bg-transparent'>
-                  Implemented
-                </Badge>
-              </span>
-              <p className={cn('mt-2 text-2xl ', font_2.className)}>
-                It is a console level productivity platform that lets you add
-                task, delete them, mark the task as completed and also provides
-                a pomodoro timer.
-              </p>
-              <div className='absolute bottom-16 flex items-center justify-start'>
-                <Button
-                  className={cn(
-                    'flex items-center justify-center gap-1 bg-[#222222] p-4 text-xl',
-                    font_2.className
-                  )}
-                >
-                  <Link href='https://rb.gy/1w8oko'>Link</Link>
-                  <ArrowUpRight size={25} />
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
-            <div className='mt-10 flex h-full w-full flex-col items-center justify-center p-6 text-[#FFF5EB]'>
-              <div className='flex w-full items-center justify-start'>
-                <h1 className={cn('text-5xl ', font_3.className)}>
-                  Weather Application
-                </h1>
-              </div>
+        <div className='grid h-full place-items-center gap-6 sm:w-[80%] xl:grid-cols-2 2xl:grid-cols-3'>
+          {cardContent.map(items => (
+            <>
+              {items.title === 'Weather Application' ? (
+                <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
+                  <div className='mt-10 flex h-full w-full flex-col items-center justify-center p-6 text-[#FFF5EB]'>
+                    <div className='flex w-full items-center justify-start'>
+                      <h1 className={cn('text-5xl ', font_2.className)}>
+                        {items.title}
+                      </h1>
+                    </div>
 
-              <span
-                className={cn(
-                  'mt-3 flex w-full items-center justify-start gap-1',
-                  font_5.className
-                )}
-              >
-                <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>Earlier</Badge>
-                <Badge className='border border-[#FFF5EB] bg-transparent'>
-                  Implemented
-                </Badge>
-              </span>
-              <p className={cn('mt-2 text-2xl ', font_2.className)}>
-                It is a python application made to understand the working of
-                API. OpenWeather API was used to generate a response and was
-                displayed over a UI made with Tkinter python module.
-              </p>
-            </div>
-          </div>
+                    <span
+                      className={cn(
+                        'mt-3 flex w-full items-center justify-start gap-1',
+                        font_3.className
+                      )}
+                    >
+                      <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>
+                        {items.badge_1}
+                      </Badge>
+                      <Badge className='border border-[#FFF5EB] bg-transparent'>
+                        {items.badge_2}
+                      </Badge>
+                    </span>
+                    <p className={cn('mt-2 text-2xl', font_1.className)}>
+                      {items.cardDescription}
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                <div className='h-[80vh] w-full rounded-3xl bg-[#FF4242]'>
+                  {items.imgSrc ? (
+                    <Image
+                      src={items.imgSrc}
+                      alt='image'
+                      className='h-1/2 rounded-t-2xl object-cover'
+                    />
+                  ) : (
+                    ''
+                  )}
+                  <div className='relative h-1/2 px-6 text-[#FFF5EB]'>
+                    <h1 className={cn('mt-10 text-5xl', font_2.className)}>
+                      {items.title}
+                    </h1>
+                    <span
+                      className={cn(
+                        'my-2 flex items-center justify-start gap-1',
+                        font_3.className
+                      )}
+                    >
+                      <Badge className=' bg-[#FFF5EB] text-[#FF4242]'>
+                        {items.badge_1}
+                      </Badge>
+                      <Badge className='border border-[#FFF5EB] bg-transparent'>
+                        {items.badge_2}
+                      </Badge>
+                    </span>
+                    <p className={cn('mt-2 text-2xl ', font_1.className)}>
+                      {items.cardDescription}
+                    </p>
+                    {items.link ? (
+                      <div className='absolute bottom-16 flex items-center justify-start'>
+                        <Button
+                          className={cn(
+                            'flex items-center justify-center gap-1 bg-[#222222] p-4 text-xl',
+                            font_1.className
+                          )}
+                        >
+                          <Link href='https://rb.gy/drdbtt'>Link</Link>
+                          <ArrowUpRight size={25} />
+                        </Button>
+                      </div>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                </div>
+              )}
+            </>
+          ))}
         </div>
       </div>
     </section>
   )
 }
 
-export default page
+export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import { Button } from '@/components/ui/button'
 import { ArrowUpRight, Download, Menu } from 'lucide-react'
 import Link from 'next/link'
 
-const font_2 = Satoshi_Regular({
+const font_1 = Satoshi_Regular({
   src: '../../fonts/Satoshi-Regular.woff'
 })
-const font_3 = CabinetGrotesk({
-  src: '../../fonts/CabinetGrotesk-Extrabold.woff'
+const font_2 = CabinetGrotesk({
+  src: '../../fonts/WEB/Gonzaga-Trial-Black.woff'
 })
 
-const font_5 = Satoshi_Bold({
+const font_3 = Satoshi_Bold({
   src: '../../fonts/Satoshi-Bold.woff'
 })
 
@@ -27,7 +27,7 @@ const Page = () => {
       {showElement && (
         <div className='absolute z-50 h-full w-full bg-[#222222]/40 p-8 backdrop-blur'>
           <div className='absolute right-10 top-20 m-4 text-[#FFF5EB] '>
-            <ul className={cn('space-y-1 text-3xl ', font_5.className)}>
+            <ul className={cn('space-y-1 text-3xl ', font_3.className)}>
               <li className='text-[#FFF5EB] hover:cursor-default hover:text-white hover:underline'>
                 <Link href={'/Project'}>Projects</Link>
               </li>
@@ -38,9 +38,9 @@ const Page = () => {
           </div>
         </div>
       )}
-      <section className='h-[100vh] p-8'>
-        <div className='relative flex h-full items-center justify-center'>
-          <div className='absolute right-0 top-0 z-50 m-4 flex w-full justify-end'>
+      <section className='p-8'>
+        <div className='relative flex h-full items-center justify-center lg:h-[90vh]'>
+          <div className='absolute right-0 top-0 z-50 m-4 flex justify-end'>
             <Button
               className='bg-transparent hover:bg-transparent'
               onClick={() => {
@@ -53,38 +53,41 @@ const Page = () => {
               />
             </Button>
           </div>
-          <div className='relative flex h-full flex-col items-center justify-center bg-[#FF4242] md:w-[70%]'>
+          <div className='relative top-10 flex h-full flex-col items-center justify-center md:top-0 md:w-[70%]'>
             <div className='relative flex h-full w-full flex-col items-center justify-center  py-8'>
-              <p
+              <div
                 className={cn(
-                  'max-w-screen-lg p-8 text-xl leading-normal text-[#FFF5EB] md:text-3xl ',
-                  font_2.className
+                  'max-w-screen-lg p-8 text-2xl leading-relaxed text-[#FF4242] lg:text-4xl ',
+                  font_1.className
                 )}
               >
                 Hi! myself
                 <br />
-                <span className={cn('text-7xl', font_3.className)}>
+                <span className={cn('text-7xl', font_2.className)}>
                   sankalp sandip pujare,{' '}
                 </span>
-                <br />a computer science student developing{' '}
-                <span className={cn(font_5.className)}>
-                  full stack applications,{' '}
-                </span>
-                and focused on creating{' '}
-                <span className={cn(font_5.className)}>
-                  innovative, efficient, and impactful{' '}
-                </span>
-                software solutions.
-                <Button
-                  className={cn(
-                    'mt-5 flex items-center justify-center gap-1 bg-[#222222] p-4 text-xl',
-                    font_2.className
-                  )}
-                >
-                  <Link href='https://rb.gy/7jrv2t'>Resume</Link>
-                  <ArrowUpRight size={25} />
-                </Button>
-              </p>
+                <br />
+                <div className='mt-3 leading-snug'>
+                  a computer science student developing{' '}
+                  <span className={cn(font_3.className)}>
+                    full stack applications,{' '}
+                  </span>
+                  and focused on creating{' '}
+                  <span className={cn(font_3.className)}>
+                    innovative, efficient, and impactful{' '}
+                  </span>
+                  software solutions.
+                  <Button
+                    className={cn(
+                      'mt-5 flex items-center justify-center gap-1 bg-[#FF4242] p-4 text-xl hover:border hover:border-[#FF4242] hover:bg-transparent hover:text-[#FF4242]',
+                      font_3.className
+                    )}
+                  >
+                    <Link href='https://rb.gy/7jrv2t'>Resume</Link>
+                    <ArrowUpRight size={26} />
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
